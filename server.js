7c2cfdd10e006a4d032dfa3edc7fd6b01f7ab2c4
viesturs <viesturs.berzins@hotmail.com>
2020-11-22 21:43:36 +0200

    Simple server and navigation

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs = require('fs').promises;
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+  console.log(req.url)
+
+  if (req.url.includes('/test')) {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(otherIndexFile);
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(indexFile);
+  }
+};
+
+fs.readFile(__dirname + "/index.html")
+  .then(contents => {
+    indexFile = contents;
+  })
+  .catch(err => {
+    console.error(`Could not read index.html file: ${err}`);
+    process.exit(1);
+  });
+
+fs.readFile(__dirname + "/test.html")
+  .then(contents => {
+    otherIndexFile = contents;
+  })
+  .catch(err => {
+    console.error(`Could not read test.html file: ${err}`);
+    process.exit(1);
+  });
+
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
